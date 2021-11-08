ba2833239f2ec72bdfeff110b70f3e9283f94e51
catejst <62929773+catejst@users.noreply.github.com>
2021-11-08 20:28:37 +0200

    Update Application.java

--- src/main/java/io/shiftleft/Application.java
+++ src/main/java/io/shiftleft/Application.java
@@ -8,8 +8,11 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+1/56991353579679:9cf301bfb55098d37f71e0c3bb764485
+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
+
 	}
 
 }
 
-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
